4ff618e452c6afd9658bef94e415b8751455d86a
Your Name <husseinsaad98@gmail.com>
2020-11-11 01:23:07 -0800

    importing project

---
+++ BIO/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+
+cloudTranslation({
+    defaultLanguage: "en",
+    languages: [
+        {
+            code: "en",
+            displayName: "English"
+        },
+        {
+            code: "ar",
+            displayName: "العربية",
+            direction: LanguageDirection.rtl
+        }
+    ]
+});
